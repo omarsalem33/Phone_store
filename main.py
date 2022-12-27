6c2179fe7132be168ff4666fa7bf31dcef33e454
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2022-12-27 11:53:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import*
 from tkinter import ttk
 import datetime
 import openpyxl
@@ -25,7 +25,7 @@ ws["D1"] = 'Total'
 ws["E1"] = 'Date Buy'
 
 wb.save('customer.xlsx')
-
+#-------------------------------------------
 
 def save():
     name = En_Name.get()
@@ -61,7 +61,7 @@ def calc():
     global En_Name, En_Address, En_Date, En_Phone, En_Total
     lb_image.place(x=950,y=438, width=250 , height=110)
     root.geometry('1200x552')
-    F4 = Frame(root, bg='#5F7161', width=250, height=434, bd=2, relief=GROOVE)
+    F4 = Frame(root, bg='#5F7161', width=250, height=434, bd=2)
     F4.place(x=950, y=1)
 
     L_Name = Label(F4, text='Name', bg='#5F7161', fg='white')
@@ -98,13 +98,14 @@ def calc():
     total = 0
     for item in trv.get_children():  # delete all Things
         trv.delete(item)
+
     for i in range(len(sb)):
         if (int(sb[i].get()) > 0):
             price = int(sb[i].get()) * menu[i][1]
             total += price
-            myst = (str(menu[i][1]), str(sb[i].get()), str(price))
+            myst = (str(menu[i][1]), str(sb[i].get()), str(price)) #10000 , 2 , 20000
             trv.insert("", "end", iid=i, text=menu[i][0], values=myst)
-
+            # GalaxyA50 ,10000 , 2 , 20000
     final = total
     En_Total.insert('1', str(final) + 'EGP')
     En_Date.insert('1', str(date))
@@ -113,10 +114,18 @@ def calc():
 def clear():
     for i in trv.get_children():
         trv.delete(i)
-    var = IntVar(root)
-    var.set(0)
-    sb1.config(textvariable=var), sb2.config(textvariable=var),sb3.config(textvariable=var), sb4.config(textvariable=var)
-    sb5.config(textvariable=var), sb6.config(textvariable=var),sb7.config(textvariable=var), sb8.config(textvariable=var)
+        
+    var1 = IntVar() 
+    var2 = IntVar()
+    var3 = IntVar() 
+    var4 = IntVar()
+    var5 = IntVar() 
+    var6 = IntVar()
+    var7 = IntVar() 
+    var8 = IntVar()
+    var1.set(0),var2.set(0),var3.set(0),var4.set(0),var5.set(0),var6.set(0),var7.set(0),var7.set(0)
+    sb1.config(textvariable=var1), sb2.config(textvariable=var2),sb3.config(textvariable=var3), sb4.config(textvariable=var4)
+    sb5.config(textvariable=var5), sb6.config(textvariable=var6),sb7.config(textvariable=var7), sb8.config(textvariable=var8)
     En_Phone.delete('0', END)
     En_Address.delete('0', END)
     En_Date.delete('0', END)
@@ -150,10 +159,10 @@ img_menu8 = PhotoImage(file='D:\FCI\Dyanamic Languages\Project\Image\iphone 8 pl
 title = Label(F1, text='Mobile Store' , font=('Tajawal 13') ,fg='white' , bg='#5F7161',width=70)
 title.place(x=0,y=0)
 #7--45
-menu1 = Button(F1 ,width=70,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu1 ,text= 'galaxyA50' ,compound=TOP)
+menu1 = Button(F1 ,width=70,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu1 ,text= 'GalaxyA50' ,compound=TOP)
 menu1.place(x=30,y=45)
 
-menu2 = Button(F1 ,width=70,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu2 ,text= 'googlePixal' ,compound=TOP)
+menu2 = Button(F1 ,width=70,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu2 ,text= 'GooglePixal' ,compound=TOP)
 menu2.place(x=170,y=45)
 
 menu3 = Button(F1 ,width=73,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu3 ,text= 'HuaweiMate20' ,compound=TOP)
@@ -165,13 +174,13 @@ menu4.place(x=450,y=45)
 menu5 = Button(F1 ,width=70,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu5 ,text= 'Honor' ,compound=TOP)
 menu5.place(x=30,y=267)
 
-menu6 = Button(F1 ,width=70,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu6 ,text= 'iphone 13 pro' ,compound=TOP)
+menu6 = Button(F1 ,width=70,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu6 ,text= 'Iphone 13 pro' ,compound=TOP)
 menu6.place(x=170,y=267)
 
-menu7 = Button(F1 ,width=73,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu7 ,text= 'iphone14 pro' ,compound=TOP)
+menu7 = Button(F1 ,width=73,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu7 ,text= 'Iphone14 pro' ,compound=TOP)
 menu7.place(x=310,y=267)
 
-menu8 = Button(F1 ,width=73,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu8 ,text= 'iphone8 plus' ,compound=TOP)
+menu8 = Button(F1 ,width=73,bg='#C0C0C0' , bd=1 , relief=FLAT ,cursor='hand2' , height=170,image=img_menu8 ,text= 'Iphone8 plus' ,compound=TOP)
 menu8.place(x=450,y=267)
 
 
@@ -196,7 +205,7 @@ sv3 =IntVar()
 sv4 =IntVar()
 sv5 =IntVar()
 sv6 =IntVar()
-sv7 =IntVar()
+sv7 =IntVar() 
 sv8 =IntVar()
 
 sb1 = Spinbox(F1, from_=0 ,to_=5, font=font1,justify="center", width=8, textvariable=sv1)
